4690e343a832d701f9fd2a7c17adfbd094089ec6
server <server@paas.uzcloud.uz>
2017-03-16 20:10:20 +0500

    yet another fix on main.py

--- app/main.py
+++ app/main.py
@@ -2,4 +2,4 @@
 import subprocess
 
 if __name__ == '__main__':
-    subprocess.check_output(['/bin/bash', 'entrypoint.sh'], stderr=subprocess.STDOUT)
+    subprocess.check_call(['/bin/bash', 'entrypoint.sh'], stderr=subprocess.STDOUT)
