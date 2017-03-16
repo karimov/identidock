09787587906a5b00e95b2a3e90fe9bc27fa74e6f
server <server@paas.uzcloud.uz>
2017-03-16 17:49:24 +0500

    modified version of main.py

--- app/main.py
+++ app/main.py
@@ -2,4 +2,4 @@
 import subprocess
 
 if __name__ == '__main__':
-    subprocess.check_out(['/bin/bash', 'entypoint.sh'], strerr=subprocess.STDOUT)
+    subprocess.check_output(['/bin/bash', 'entypoint.sh'], strerr=subprocess.STDOUT)
