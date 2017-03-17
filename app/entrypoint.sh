4b1dc4e17274917a13329bbec6ded7869d12b7bb
server <server@paas.uzcloud.uz>
2017-03-17 13:02:56 +0500

    fix without tests.py

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -6,10 +6,12 @@ echo "$ENV"
 echo "------------------"
 pip install -r 'requirments.txt'
 
-if [ "$ENV" = 'DEV' ]; then
-    echo "Running DevVersion ..."
-    exec python identidock.py
-else
-    echo "Running UnitTest version ..."
-    exec python tests.py
-fi
+python identidock.py
+
+#if [ "$ENV" = 'DEV' ]; then
+#    echo "Running DevVersion ..."
+#    exec python identidock.py
+#else
+#    echo "Running UnitTest version ..."
+#    exec python tests.py
+#fi
